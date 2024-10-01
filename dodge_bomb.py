709ad6a3894d10cb942ed9183b919025ce4902cf
koizumitmy <c0b2306162@edu.teu.ac.jp>
2024-10-01 17:05:02 +0900

    演習２：超強拡大爆破弾

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,6 +4,7 @@ import sys
 import pygame as pg
 import time
 
+accs = [a for a in range(1, 11)]
 
 WIDTH, HEIGHT = 1100, 650
 DELTA = {
@@ -28,6 +29,16 @@ def check_bpund(obj_rct:pg.Rect) -> tuple[bool, bool]:
         tate = False 
     return yoko, tate
 
+def create_bomb():
+    accs = [a for a in range(1, 11)]
+    bb_imgs = []
+    for r in range(1, 11):
+        bb_img = pg.Surface((20*r, 20*r))
+        pg.draw.circle(bb_img, (255, 0, 0), (10*r, 10*r), 10*r)
+        bb_img.set_colorkey((0,0,0))
+        bb_imgs.append(bb_img)
+    return accs, bb_imgs   
+
 
 
 def main():
@@ -41,10 +52,12 @@ def main():
 
     bb_img = pg.Surface((20,20)) # 空のSurface
     pg.draw.circle(bb_img,(255, 0, 0), (10, 10), 10)
-    bb_img.set_colorkey((0,0,0))
+    vx, vy = +5, -5
+    
     bb_rct = bb_img.get_rect() # 爆弾rectの抽出
     bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
-    vx, vy = +5, -5
+
+    accs, bb_imgs = create_bomb()
 
     clock = pg.time.Clock()
     tmr = 0
@@ -57,17 +70,18 @@ def main():
         screen.blit(bg_img, [0, 0]) 
 
         if kk_rct.colliderect(bb_rct): # こうかとんと爆弾が重なった場合
-            backg = pg.Surface((1100, 650))
+            backg = pg.Surface((1100, 650)) # 半透明の黒背景を描画
             backg.set_alpha(128)
             backg.fill((0, 0, 0))
             screen.blit(backg, (0, 0))
-            fonto = pg.font.Font(None, 80)
+            fonto = pg.font.Font(None, 80) #GameOverの文字を描画
             txt = fonto.render("Game Over!", True, (255, 255, 255))
             screen.blit(txt, [400, 200])
-            screen.blit(kkc_img,[345, 200])
+            screen.blit(kkc_img,[345, 200]) #泣いているこうかとんを描画
             screen.blit(kkc_img, [745,200])
-            pg.display.update()
-            time.sleep(5)
+            pg.display.update() # 画面の更新
+            time.sleep(5) #　時間の一時停止
+            return
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0] # 横座標, 縦座標
@@ -88,16 +102,19 @@ def main():
 
         kk_rct.move_ip(sum_mv)
         
+        avx = vx*accs[min(tmr//500, 9)] # 位置の加速度
+        avy = vy*accs[min(tmr//500, 9)]
+
         if check_bpund(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
-        screen.blit(kk_img, kk_rct)    
-
-        bb_rct.move_ip((vx, vy))
+        screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip((avx, avy))
         yoko, tate = check_bpund(bb_rct)
         if not yoko:
             vx *= -1
         elif not tate :
             vy *= -1
+        bb_img = bb_imgs[min(tmr//500, 9)] # こうかとんの大きさ拡大
         screen.blit(bb_img,bb_rct)
         pg.display.update()
         tmr += 1
