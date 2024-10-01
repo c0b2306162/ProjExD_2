3c77c5d8f76a4cf92e4a63c7594d974d65512a0f
koizumitmy <c0b2306162@edu.teu.ac.jp>
2024-10-01 14:38:01 +0900

    練習２：爆弾設定の作成

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,4 +1,5 @@
 import os
+import random
 import sys
 import pygame as pg
 
@@ -20,6 +21,14 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+
+    bb_img = pg.Surface((20,20)) # 空のSurface
+    pg.draw.circle(bb_img,(255, 0, 0), (10, 10), 10)
+    bb_img.set_colorkey((0,0,0))
+    bb_rct = bb_img.get_rect() # 爆弾rectの抽出
+    bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+    vx, vy = +5, -5
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -39,7 +48,7 @@ def main():
         #     sum_mv[0] -= 5
         # if key_lst[pg.K_RIGHT]:
         #     sum_mv[0] += 5
-        
+
         for key, tpl in DELTA.items():
             if key_lst[key]:
                 sum_mv[0] += tpl[0] # 横方向
@@ -47,6 +56,8 @@ def main():
 
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip((vx, vy))
+        screen.blit(bb_img,bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
